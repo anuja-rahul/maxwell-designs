8cfe6ad7c3d377bcf966d501a5d0ae4ce67d0e6b
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-06-05 01:28:59 +0530

    added shapes

---
+++ src/components/shapes/Shapes.tsx
@@ -0,0 +1,117 @@
+import Shape_01 from "./../../assets/shapes/shape-1.svg";
+import Shape_02 from "./../../assets/shapes/shape-2.svg";
+import Shape_03 from "./../../assets/shapes/shape-3.svg";
+import Shape_04 from "./../../assets/shapes/shape-4.svg";
+import Shape_05 from "./../../assets/shapes/shape-5.svg";
+import Shape_06 from "./../../assets/shapes/shape-6.svg";
+import Shape_07 from "./../../assets/shapes/shape-7.svg";
+import Shape_08 from "./../../assets/shapes/shape-8.svg";
+import Shape_09 from "./../../assets/shapes/shape-9.svg";
+import Shape_10 from "./../../assets/shapes/shape-10.svg";
+import Shape_11 from "./../../assets/shapes/shape-11.svg";
+import Shape_12 from "./../../assets/shapes/shape-12.svg";
+import Shape_13 from "./../../assets/shapes/shape-13.svg";
+import Shape_14 from "./../../assets/shapes/shape-14.svg";
+import Shape_15 from "./../../assets/shapes/shape-15.svg";
+import Shape_16 from "./../../assets/shapes/shape-16.svg";
+import Shape_17 from "./../../assets/shapes/shape-17.svg";
+import Shape_18 from "./../../assets/shapes/shape-18.svg";
+import Shape_19 from "./../../assets/shapes/shape-19.svg";
+import Shape_20 from "./../../assets/shapes/shape-20.svg";
+
+import Image from "next/image";
+import clsx from "clsx";
+
+interface shapeProps {
+  className?: string;
+  extras?: string;
+}
+
+export const Shape01 = ({ className}: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_01}
+      alt="shape_01"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape02 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_02}
+      alt="shape_02"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape03 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_03}
+      alt="shape_03"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape04 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_04}
+      alt="shape_04"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape05 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_05}
+      alt="shape_05"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape06 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_06}
+      alt="shape_06"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape07 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_07}
+      alt="shape_07"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
+export const Shape08 = ({ className }: shapeProps) => {
+  return (
+    <Image
+      priority
+      src={Shape_08}
+      alt="shape_08"
+      className={clsx("", className)}
+      width={100}
+    />
+  );
+};
